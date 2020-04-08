387c844ab034cea7e53f86007fe3500959478a27
Alexander Sakhaev <alsakhaev@gmail.com>
2020-04-08 12:55:52 +0300

    add config with secrets

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,21 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+const config = require('./config.json');
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*"
     },
     test: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*"
     },
     rinkeby: {
       provider: function () {
-        return new HDWalletProvider('', "https://rinkeby.infura.io/v3/API_KEY");
+        return new HDWalletProvider(config.rinkeby_mnemonic, `https://rinkeby.infura.io/v3/${config.rinkeby_infura_api_key}`);
       },
       network_id: '4',
       gas: 10000000
