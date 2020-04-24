b7a54ec4d9fd607b589495a890bd48bde33bcc51
Alexander Sakhaev <alsakhaev@gmail.com>
2020-04-24 15:22:12 +0300

    BREAKING CHANGE: content hash and URIs mapping

--- contracts/DappletRegistry.sol
+++ contracts/DappletRegistry.sol
@@ -5,7 +5,13 @@ pragma experimental ABIEncoderV2;
 // ToDo: maybe hash is better to use?
 // hash(name+branch, version)
 
+/// @author Dapplets Team
+/// @title Dapplet Registry
 contract DappletRegistry {
+    /////////////////////////////////////
+    // MODULES REGISTRY
+    /////////////////////////////////////
+
     struct ModuleNameInfo {
         address owner;
         mapping(string => ModuleBranchInfo) infoByBranches;
@@ -13,147 +19,303 @@ contract DappletRegistry {
 
     struct ModuleBranchInfo {
         string[] versions; // ToDo: fix double storing of versions
-        mapping(string => string[]) urisByVersion;
+        mapping(string => bytes32) hashByVersion;
     }
 
-    struct AddModuleInput {
-        string name;
-        string branch;
-        string version;
-        string uri;
-    }
+    // // ToDo: read operations are free, because of it's better to optimize writing.
+    // // Low level: 1) call, 2) send tx
 
-    // ToDo: read operations are free, because of it's better to optimize writing.
-    // Low level: 1) call, 2) send tx
+    // // ToDo: does public accessor generate setter?
+    mapping(string => ModuleNameInfo) public infoByName;
 
-    // ToDo: does public accessor generate setter?
-    mapping(string => ModuleNameInfo) private _infoByName;
-
-    // ToDo: fix double storing of [name, branch]
-    mapping(string => string[]) private _modulesByLocation;
-
+    /// Get versions of specific module's branch
+    /// @param name name of module
+    /// @param branch branch of module
+    /// @return array of module versions
     function getVersions(string memory name, string memory branch)
         public
         view
         returns (string[] memory)
     {
-        return _infoByName[name].infoByBranches[branch].versions;
+        return infoByName[name].infoByBranches[branch].versions;
     }
 
-    function resolveToUri(
+    /// Resolve module to URIs
+    /// @param name name of module
+    /// @param branch branch of module
+    /// @param version branch of module
+    /// @return hash keccak-256 hash of module's manifest
+    /// @return uris array of URIs
+    function resolveToUris(
         string memory name,
         string memory branch,
         string memory version
-    ) public view returns (string[] memory) {
-        return _infoByName[name].infoByBranches[branch].urisByVersion[version];
+    ) public view returns (bytes32 hash, string[] memory uris) {
+        return (
+            infoByName[name].infoByBranches[branch].hashByVersion[version],
+            urisByHash[hash].uris
+        );
     }
 
-    // ToDo: location must be array of strings
-    // ToDo: how many modules can be returned?
-    // ToDo: is it need to be optimized?
+    struct HashUri {
+        bytes32 hash;
+        string uri;
+    }
+
+    /// Add module with manifest hash. It's irreversible operation
+    /// @param name name of module to be added
+    /// @param branch branch of module to be added
+    /// @param version version of module to be added
+    /// @param hashUris keccak-256 hashes and URIs of module's objects. First element of array must be a manifest.
+    function addModuleWithObjects(
+        string memory name,
+        string memory branch,
+        string memory version,
+        HashUri[] memory hashUris
+    ) public {
+        require(
+            hashUris.length > 0,
+            "Module must have objects with hashes and URIs."
+        );
+
+        // object registration
+        for (uint256 i = 0; i < hashUris.length; i++) {
+            addHashUri(hashUris[i].hash, hashUris[i].uri);
+        }
+
+        // module registration
+        bytes32 manifestHash = hashUris[0].hash;
+        addModule(name, branch, version, manifestHash);
+    }
+
+    struct AddModulesWithObjectsInput {
+        string name;
+        string branch;
+        string version;
+        HashUri[] hashUris;
+    }
+
+    /// Batch call of addModuleWithObjects() function
+    /// @param input array of addModuleWithObjects() parameters
+    function addModulesWithObjects(AddModulesWithObjectsInput[] memory input)
+        public
+    {
+        for (uint256 i = 0; i < input.length; i++) {
+            addModuleWithObjects(
+                input[i].name,
+                input[i].branch,
+                input[i].version,
+                input[i].hashUris
+            );
+        }
+    }
+
+    event ModuleAdded(
+        string name,
+        string branch,
+        string version,
+        bytes32 manifestHash
+    );
+
+    /// Add module with manifest hash. It's irreversible operation
+    /// @param name name of module to be added
+    /// @param branch branch of module to be added
+    /// @param version version of module to be added
+    /// @param manifestHash keccak-256 hash of module's manifest
+    function addModule(
+        string memory name,
+        string memory branch,
+        string memory version,
+        bytes32 manifestHash
+    ) public {
+        // module ownership checking
+        require(
+            infoByName[name].owner == address(0x0) ||
+                msg.sender == infoByName[name].owner,
+            "This action can be done only by module's owner."
+        );
+
+        // manifest ownership checking
+        require(
+            urisByHash[manifestHash].owner == msg.sender,
+            "Can not register a module with a foreign manifest."
+        );
+
+        // owning
+        if (infoByName[name].owner == address(0x0)) {
+            infoByName[name].owner = msg.sender;
+        }
+
+        ModuleBranchInfo storage info = infoByName[name].infoByBranches[branch];
+
+        if (info.hashByVersion[version] == bytes32(0x0)) {
+            info.versions.push(version);
+            info.hashByVersion[version] = manifestHash;
+        }
+
+        // ToDo: check previous versions
+
+        emit ModuleAdded(name, branch, version, manifestHash);
+    }
+
+    struct AddModulesInput {
+        string name;
+        string branch;
+        string version;
+        bytes32 manifestHash;
+    }
+
+    /// Batch call of addModule() function
+    /// @param input array of addModule() parameters
+    function addModules(AddModulesInput[] memory input) public {
+        for (uint256 i = 0; i < input.length; i++) {
+            addModule(
+                input[i].name,
+                input[i].branch,
+                input[i].version,
+                input[i].manifestHash
+            );
+        }
+    }
+
+    /////////////////////////////////////
+    // LOCATIONS
+    /////////////////////////////////////
+
+    mapping(string => string[]) public modulesByLocation;
+
+    /// Return module names by location
+    /// @param location identifier of context type
+    /// @return array of module names
     function getModules(string memory location)
         public
         view
         returns (string[] memory)
     {
-        return _modulesByLocation[location];
+        // ToDo: location must be array of strings
+        // ToDo: how many modules can be returned?
+        // ToDo: is it need to be optimized?
+
+        return modulesByLocation[location];
     }
 
-    event ModuleAdded(string name, string branch, string version, string uri);
-
-    function addModule(
-        string memory name,
-        string memory branch,
-        string memory version,
-        string memory uri
-    ) public {
-        // ownership checking
+    /// Add location for module
+    /// @param moduleName name of module
+    /// @param location location of module to be added
+    function addLocation(string memory moduleName, string memory location)
+        public
+    {
         require(
-            _infoByName[name].owner == address(0x0) ||
-                msg.sender == _infoByName[name].owner,
-            "This action can be done only by module's owner."
-        );
-
-        // owning
-        if (_infoByName[name].owner == address(0x0)) {
-            _infoByName[name].owner = msg.sender;
-        }
-
-        ModuleBranchInfo storage info = _infoByName[name]
-            .infoByBranches[branch];
-
-        uint256 urisCount = info.urisByVersion[version].length;
-
-        if (urisCount == 0) {
-            info.versions.push(version);
-        }
-
-        // ToDo: check previous versions
-
-        // ToDo: check existence of the uri? Dima: create map uri => boolean;
-        info.urisByVersion[version].push(uri);
-
-        emit ModuleAdded(name, branch, version, uri);
-    }
-
-    function addModules(AddModuleInput[] memory modules) public {
-        for (uint256 i = 0; i < modules.length; i++) {
-            addModule(
-                modules[i].name,
-                modules[i].branch,
-                modules[i].version,
-                modules[i].uri
-            );
-        }
-    }
-
-    function addLocation(string memory name, string memory location) public {
-        require(
-            _infoByName[name].owner != address(0x0),
+            infoByName[moduleName].owner != address(0x0),
             "No modules with this name exist"
         );
         require(
-            _infoByName[name].owner == msg.sender,
+            infoByName[moduleName].owner == msg.sender,
             "This action can be done only by module's owner"
         );
 
         // ToDo: check empty strings everywhere
-        _modulesByLocation[location].push(name);
+        modulesByLocation[location].push(moduleName);
     }
 
-    struct AddLocationInput {
-        string name;
+    struct AddLocationsInput {
+        string moduleName;
         string location;
     }
 
-    function addLocations(AddLocationInput[] memory locations) public {
-        for (uint256 i = 0; i < locations.length; i++) {
-            addLocation(locations[i].name, locations[i].location);
+    /// Batch call of addLocation() function
+    /// @param input array of addLocation() parameters
+    function addLocations(AddLocationsInput[] memory input) public {
+        for (uint256 i = 0; i < input.length; i++) {
+            addLocation(input[i].moduleName, input[i].location);
         }
     }
 
-    function removeLocation(string memory name, string memory location) public {
+    /// Remove location of module
+    /// @param moduleName name of module
+    /// @param location location of module to be removed
+    function removeLocation(string memory moduleName, string memory location)
+        public
+    {
         require(
-            _infoByName[name].owner != address(0x0),
+            infoByName[moduleName].owner != address(0x0),
             "No modules with this name exist"
         );
         require(
-            _infoByName[name].owner == msg.sender,
+            infoByName[moduleName].owner == msg.sender,
             "This action can be done only by module's owner"
         );
 
-        string[] storage modules = _modulesByLocation[location];
+        string[] storage modules = modulesByLocation[location];
 
-        // ToDo: how to optimize it? Dima: use map
+        // ToDo: optimize it (map?)
         for (uint256 i = 0; i < modules.length; i++) {
-            if (areEqual(modules[i], name)) {
+            if (areEqual(modules[i], moduleName)) {
                 modules[i] = modules[modules.length - 1];
-                modules.pop(); // ToDo: or delete _modulesByLocation[location][length - 1] ? Dima: delete maybe just make it empty.
+                modules.pop();
                 break;
             }
         }
     }
 
+    /////////////////////////////////////
+    // HASH-URI REGISTRY
+    /////////////////////////////////////
+
+    struct OwnedUris {
+        address owner;
+        string[] uris; // ToDo: maybe limited array to use here
+    }
+
+    mapping(bytes32 => OwnedUris) public urisByHash;
+
+    /// Resolve object's hash to URIs
+    /// @param hash keccak-256 hash of object
+    /// @return array of URIs
+    function hashToUris(bytes32 hash) public view returns (string[] memory) {
+        return urisByHash[hash].uris;
+    }
+
+    /// Add URI for object's hash
+    /// @param hash keccak-256 hash of object
+    /// @param uri URI of object
+    function addHashUri(bytes32 hash, string memory uri) public {
+        require(
+            urisByHash[hash].owner == address(0x0) ||
+                msg.sender == urisByHash[hash].owner,
+            "This action can be done only by object's owner"
+        );
+
+        urisByHash[hash].uris.push(uri);
+    }
+
+    /// Remove URI of object's hash by its array index
+    /// @param hash keccak-256 hash of object
+    /// @param uriIndex index of URI in urisByHash[hash].uris array
+    function removeHashUri(bytes32 hash, uint256 uriIndex) public {
+        require(
+            urisByHash[hash].owner != address(0x0),
+            "No objects with this hash exist"
+        );
+        require(
+            urisByHash[hash].owner == msg.sender,
+            "This action can be done only by object's owner"
+        );
+
+        string[] storage uris = urisByHash[hash].uris;
+
+        // URI removing by index
+        uris[uriIndex] = uris[uris.length - 1];
+        uris.pop();
+    }
+
+    /////////////////////////////////////
+    // UTILITIES
+    /////////////////////////////////////
+
+    /// Compare strings
+    /// @param a first string
+    /// @param b second string
     function areEqual(string memory a, string memory b)
         public
         pure
