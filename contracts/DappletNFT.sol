dbff68b0609507f0cfccd0b6f3c1bf073eac02ec
Alexander Sakhaev <alsakhaev@gmail.com>
2022-08-18 21:26:50 +0300

    feat: implement nft metadata uri

--- contracts/DappletNFT.sol
+++ contracts/DappletNFT.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import {DappletRegistry, ModuleInfo} from "./DappletRegistry.sol";
 
 contract DappletNFT is ERC721, ERC721Enumerable, Ownable {
+    using Strings for uint8;
+
     constructor() ERC721("Dapplets NFTs", "DNFTs") {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
@@ -61,4 +66,48 @@ contract DappletNFT is ERC721, ERC721Enumerable, Ownable {
             dappIndxs[i] = tokenOfOwnerByIndex(owner, i + offset);
         }
     }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        DappletRegistry registry = DappletRegistry(owner());
+        (ModuleInfo[] memory modules,,,) = registry.getModules(tokenId, 1);
+        ModuleInfo memory module = modules[0];
+
+        string memory description = string(abi.encodePacked(
+            'This NFT is a proof of ownership of the \\"', module.title, '\\".\\n',
+            module.description, '\\n',
+            'This dapplet is a part of the Dapplets Project ecosystem for augmented web. All dapplets are available in the Dapplets Store.'
+        ));
+
+        bytes memory dataURI = abi.encodePacked(
+            '{',
+                '"name":"Dapplet \\"', module.title, '\\"",',
+                '"description":"', description, '",'
+                '"attributes":[{',
+                    '"trait_type":"Name",', 
+                    '"value":"', module.name, '"',
+                '},{',
+                    '"trait_type":"Module Type",', 
+                    '"value":"', module.moduleType.toString(), '"',
+                '}]',
+                // Replace with extra ERC721 Metadata properties
+            '}'
+        );
+
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(dataURI)
+            )
+        );
+    }
+
+    // ToDo: implement contract metadata
+    // function contractURI() public view returns (string memory) {
+    //     return "";
+    // }
 }
